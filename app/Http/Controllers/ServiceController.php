7d7a220d7153b6cdeb882b0f982c69eadeac2d5f
Nguru <billnguts@gmail.com>
2019-08-01 23:22:46 +0300

    [fix] /service  with centre rship

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -15,7 +15,7 @@ class ServiceController extends Controller
      */
     public function index()
     {
-        $services = Service::with('deviceinfo.centre')->get();
+        $services = Service::with('centre')->get();
         return response()->json($services);
     }
 
